69c0a6264d839829635b1f9d140b58f4ac255727
Damon Edmons <dsedmons@gmail.com>
2022-10-21 11:13:41 -0400

    Feat: calling api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useCallback, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  const url =
+    "https://fa-homefax-ea2-dev-001.azurewebsites.net/api/HomeInformationByName";
+  const options = {
+    method: "GET",
+    headers: {},
+  };
+  const getData = async () => {
+    return fetch(url, options)
+      .then((response) => response.json())
+      //.then((data) => console.log(data));
+  };
+
+  const fetchData = useCallback(async () => {
+    getData().then((rawData) => {
+      setData(rawData);
+      //console.log(rawData)
+    });
+  }, [getData]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Damon & Rob
-        </p>
+        <p>Damon & Rob</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +41,8 @@ function App() {
         >
           Learn React
         </a>
+        {data && <div>{data[0].address_1}</div>}
+        {/* <div>{data && data[0].address_1}</div> */}
       </header>
     </div>
   );
